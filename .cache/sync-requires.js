943df9b49dd4b058b9c6374c0da615e8d3fb59c1
gmtteja <gangameher96@gmail.com>
2020-09-28 20:01:33 +0100

    changes

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,10 +5,10 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/gmt/bulma1/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/home/gmt/bulma1/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/home/gmt/bulma1/src/pages/index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/home/gmt/bulma1/src/pages/page-2.js"))),
-  "component---src-pages-using-typescript-tsx": hot(preferDefault(require("/home/gmt/bulma1/src/pages/using-typescript.tsx")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/gmt/bulma-/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/home/gmt/bulma-/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/home/gmt/bulma-/src/pages/index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/home/gmt/bulma-/src/pages/page-2.js"))),
+  "component---src-pages-using-typescript-tsx": hot(preferDefault(require("/home/gmt/bulma-/src/pages/using-typescript.tsx")))
 }
 
